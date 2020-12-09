fb620967048e4755c52d913669025819ffccc52e
Cristian Corrales <cristiancorrales84@gmail.com>
2020-12-08 21:28:08 -0500

    Added completed project

--- js/script.js
+++ js/script.js
@@ -10,24 +10,69 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [{quote:"This is the Way.",source:"The Armorer",citation:"Star Wars The Mandalorian", year: 2019 },{quote:"SOOGAA!", source:"Jawas"},
+{quote:"The Force will be with you. Always.", source:"Obi-Wan Kenobi"},{quote:"Do. Or do not. There is no try.", source:"Yoda", tags:"Inspirational, Motivation, Ambition"},
+{quote:"You can’t stop the change, any more than you can stop the suns from setting.", source:"Shmi Skywalker"}];
 
 
 /***
  * `getRandomQuote` function
+ * Returns a random object
+ * 
+ * @returns {object} The random object from the quotes array.
 ***/
+const getRandomQuote = () =>{
+  const rand = Math.floor(Math.random() * quotes.length);
+  return quotes[rand];
+}
 
+/***
+ * `randomColor` function
+ * Generates and returns a random rgb color value
+ * 
+ * @returns {String} The random rgb value
+***/
+const randomColor = () =>{
+  let rgb = `rgb(`;
+  const randomRed = Math.floor(Math.random() * 255);
+  const randomGreen = Math.floor(Math.random() * 255);
+  const randomBlue = Math.floor(Math.random() * 255);
 
+  rgb+= `${randomRed},${randomBlue},${randomGreen})`;
+
+  return rgb;
+
+}
 
 /***
  * `printQuote` function
+ * Prints a random quote to the document
+ * 
+ *
 ***/
+const printQuote = () =>{
+  const randomQuote = getRandomQuote();
+  let html = `<p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source}`;
 
+  if(randomQuote.citation){
+    html += `<span class='citation'>${randomQuote.citation}</span>`;
+  }
+  if(randomQuote.year){
+    html += `<span class='year'>${randomQuote.year}</span>`;
+  }
+  if(randomQuote.tags){
+    html+= `<br><span class ="tag"><small>tags: ${randomQuote.tags}</small></span>`;
+  }
 
+  html+= "</p>";
+  document.querySelector("body").style.backgroundColor = randomColor();
+  document.getElementById("quote-box").innerHTML = html;
+}
 
+setInterval(printQuote, 8000);
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
